d71dfd8dc1249246b99b85377be510c644e327e4
Gbrlzinn <144288138+gbrlalmeida@users.noreply.github.com>
2024-06-19 13:47:53 -0300

    Update bancosystem.sql

--- bancosystem.sql
+++ bancosystem.sql
@@ -4,3 +4,14 @@ CREATE TABLE academias (
     email VARCHAR(100) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL
 );
+
+
+CREATE TABLE avaliacao (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    idade INT NOT NULL,
+    peso FLOAT NOT NULL,
+    altura FLOAT NOT NULL,
+    imc FLOAT NOT NULL,
+    observacao TEXT
+);
